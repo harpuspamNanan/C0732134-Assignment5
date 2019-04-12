c8fa09847fcac084f8164be1fb4ddf7c238722c1
hps3103 <ssingh.harpushpam@gmail.com>
2019-04-12 18:00:46 -0400

    Gematria

--- C0732134-Assignment5/Program.cs
+++ C0732134-Assignment5/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace C0732134_Assignment5
 {
-    class Program
+    class Program2
     {
-        static void Main(string[] args)
+        public static string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+        public static void Main()
         {
+            Program2 orange = new Program2();
+            Console.WriteLine(orange.Gematria("universe"));
+
+            Console.ReadLine();
+        }
+
+        public int Gematria(string word)
+        {
+            int gematriaValue = 0;
+            for (int i = 0; i < word.Length; i++)
+                gematriaValue += LetterValue(word[i]);
+
+            return gematriaValue;
+        }
+
+
+
+
+        public int LetterValue(char letter)
+        {
+
+            int x = 0;
+
+            for (int i = 0; i < Letters.Length; i++)
+            {
+                if (Letters[i] == letter.ToString())
+                    x += (i+1);
+            }
+
+            return x;           
+
         }
     }
 }
